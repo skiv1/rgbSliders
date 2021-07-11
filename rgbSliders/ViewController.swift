cb3434941b0a898208ac4a1639fdd6056314dec8
Anton Grekov <grekov_anton@bk.ru>
2021-07-11 21:50:10 +0300

    слайдер передают цвета и оттенки

--- rgbSliders/ViewController.swift
+++ rgbSliders/ViewController.swift
@@ -29,16 +29,30 @@ class ViewController: UIViewController {
     // sliders
     @IBAction func sliderRedAction() {
         labelRed.text = String(format: "%.2f", sliderRed.value)
+        let sliderRedValue = CGFloat(sliderRed.value)
+        viewColour.backgroundColor = UIColor .red
+        viewColour.backgroundColor = viewColour.backgroundColor?.withAlphaComponent(sliderRedValue)
+        
     
     }
     
     @IBAction func sliderGreenAction() {
         labelGreen.text = String(format: "%.2f", sliderGreen.value)
+        let sliderGreenValue = CGFloat(sliderGreen.value)
+        viewColour.backgroundColor = UIColor .green
+        viewColour.backgroundColor = viewColour.backgroundColor?.withAlphaComponent(sliderGreenValue)
+
         
     }
     @IBAction func sliderBlueAction() {
         labelBlue.text = String(format: "%.2f", sliderBlue.value)
+        let sliderBlueValue = CGFloat(sliderBlue.value)
+        viewColour.backgroundColor = UIColor .blue
+        viewColour.backgroundColor = viewColour.backgroundColor?.withAlphaComponent(sliderBlueValue)
+
+
     }
+    
    
     
     
