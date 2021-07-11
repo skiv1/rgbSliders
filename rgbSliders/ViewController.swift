44e9bc6b82fa115bc28643218da3f823285bd069
Anton Grekov <grekov_anton@bk.ru>
2021-07-11 16:50:04 +0300

    Initial Commit

---
+++ rgbSliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rgbSliders
+//
+//  Created by Anton Grekov on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
