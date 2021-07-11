3762563b645501806b3f7834b261dc2d51096529
Anton Grekov <grekov_anton@bk.ru>
2021-07-11 22:40:00 +0300

    внес корректировку и ввел новую функцию, теперь цвет передается корректно

--- rgbSliders/ViewController.swift
+++ rgbSliders/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderRed: UISlider!
     @IBOutlet weak var sliderGreen: UISlider!
     @IBOutlet weak var sliderBlue: UISlider!
+    @IBOutlet weak var sliderRGB: UISlider!
     
     @IBOutlet weak var viewColour: UIView!
     
@@ -29,31 +30,27 @@ class ViewController: UIViewController {
     // sliders
     @IBAction func sliderRedAction() {
         labelRed.text = String(format: "%.2f", sliderRed.value)
-        let sliderRedValue = CGFloat(sliderRed.value)
-        viewColour.backgroundColor = UIColor .red
-        viewColour.backgroundColor = viewColour.backgroundColor?.withAlphaComponent(sliderRedValue)
-        
-    
+       
     }
     
     @IBAction func sliderGreenAction() {
         labelGreen.text = String(format: "%.2f", sliderGreen.value)
-        let sliderGreenValue = CGFloat(sliderGreen.value)
-        viewColour.backgroundColor = UIColor .green
-        viewColour.backgroundColor = viewColour.backgroundColor?.withAlphaComponent(sliderGreenValue)
-
         
+
     }
     @IBAction func sliderBlueAction() {
         labelBlue.text = String(format: "%.2f", sliderBlue.value)
-        let sliderBlueValue = CGFloat(sliderBlue.value)
-        viewColour.backgroundColor = UIColor .blue
-        viewColour.backgroundColor = viewColour.backgroundColor?.withAlphaComponent(sliderBlueValue)
-
-
+    
+    }
+   
+    func changeColor(){
+        viewColour.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
     }
     
    
+    @IBAction func changeColourRGB() {
+        changeColor()
+    }
     
     
 }
